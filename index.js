5e0418bac84b5ae5a5e156ef87418bcaa0a4fbdf
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-05-13 20:21:36 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var Paser = require('cst').Parser;
+
+var code = `
+({
+	42: 42
+});
+`;
+
+var tree = new Paser().parse(code);
+
+var objects = program.selectTokensByType('ObjectExpression');
+
+debugger;
+
+console.log(objects.length); // 1 ? nope it's 0
+
